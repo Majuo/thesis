23ce8f9277600499db59c14ea6a8d256df0affc6
Ilja Karpenko <ilja.karpenko@gmail.com>
2022-09-17 16:51:53 +0300

    Cleanup, refactor.

--- SudokuApp/ViewModels/MainViewModel.cs
+++ SudokuApp/ViewModels/MainViewModel.cs
@@ -4,13 +4,8 @@ namespace SudokuApp.ViewModels
 {
 	public class MainViewModel : ViewModelBase
 	{
-		public string Greeting => "Welcome to SudokuApp!";
-		public Sudoku Sudoku { get; set; }
-		public SudokuCell SelectedCell { get; set; }
-
 		public MainViewModel()
 		{
-			Sudoku = new Sudoku();
 		}
 	}
 }
---
+++ SudokuApp/Views/SudokuView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SudokuApp.Views;
+
+public partial class SudokuView : UserControl
+{
+	public SudokuView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
