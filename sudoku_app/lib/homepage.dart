873ed16e8f4e0ca14ccbdd789101d4d66e0cbe5f
Ilja Karpenko <ilja.karpenko@gmail.com>
2023-03-26 17:11:38 +0300

    remove counter

--- sudoku_app/lib/homepage.dart
+++ sudoku_app/lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:sudoku_app/widgets/sudoku_grid.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,12 +9,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-	int _counter = 0;
   SudokuGrid grid = const SudokuGrid();
 
 	void _incrementCounter() {
 		setState(() {
-		_counter++;
 		});
 	}
 
@@ -26,24 +23,9 @@ class _HomePageState extends State<HomePage> {
 				child: Column(
 					mainAxisAlignment: MainAxisAlignment.center,
 					children: <Widget>[
-						Text(
-							AppLocalizations.of(context).helloWorld
-						),
-						const Text(
-							'You have pushed the button this many times:',
-						),
-						Text(
-							'$_counter',
-							style: Theme.of(context).textTheme.headlineMedium,
-						),
 						grid,
 					],
 				),
-			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: _incrementCounter,
-				tooltip: 'Increment',
-				child: const Icon(Icons.add),
 			),
 		);
 	}
