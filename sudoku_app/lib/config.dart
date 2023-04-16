90c814a64292e158912e65e9f467abcd6eca653d
Ilja Karpenko <ilja.karpenko@gmail.com>
2023-04-16 17:51:13 +0300

    .

--- sudoku_app/lib/config.dart
+++ sudoku_app/lib/config.dart
@@ -1,7 +1,7 @@
 library config.globals;
 
 import 'package:sudoku_app/locale/current_locale.dart';
-import 'package:sudoku_app/settings/navigatio_menu_setting.dart';
+import 'package:sudoku_app/settings/navigation_menu_setting.dart';
 import 'package:sudoku_app/theme/current_theme.dart';
 
 CurrentTheme currentTheme = CurrentTheme();
