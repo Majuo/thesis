b85fd218c49a8aac084c752abd6ad31a942d7cfe
Ilja Karpenko <ilja.karpenko@gmail.com>
2023-05-11 21:29:35 +0300

    fix indentation

--- sudoku_app/lib/game_internals/solver/techniques/last_possible_number_technique.dart
+++ sudoku_app/lib/game_internals/solver/techniques/last_possible_number_technique.dart
@@ -8,22 +8,22 @@ class LastPossibleNumberTechnique implements ISudokuTechnique {
 
 	@override
 	TechniqueResult? trySolve(Sudoku sudoku, {bool applyResult = false}) {
-	for (var row in sudoku.board) {
-		for (var cell in row.where((c) => c.value == 0)) {
-			var missingNums = Sudoku.numSet.difference(sudoku.getPeerValues(cell.row, cell.col));
-			if (missingNums.length == 1) {
-				if (applyResult) {
-          cell.candidates.clear();
-				  cell.value = missingNums.first;
-          var peerCells = sudoku.getPeerCells(cell.row, cell.col);
-          for (var pc in peerCells) {
-            pc.candidates.remove(missingNums.first);
+    for (var row in sudoku.board) {
+      for (var cell in row.where((c) => c.value == 0)) {
+        var missingNums = Sudoku.numSet.difference(sudoku.getPeerValues(cell.row, cell.col));
+        if (missingNums.length == 1) {
+          if (applyResult) {
+            cell.candidates.clear();
+            cell.value = missingNums.first;
+            var peerCells = sudoku.getPeerCells(cell.row, cell.col);
+            for (var pc in peerCells) {
+              pc.candidates.remove(missingNums.first);
+            }
           }
-				}
-				return TechniqueResult(applicableCells: [SudokuCell(cell.row, cell.col, true, missingNums.first)], usedTechnique: LastPossibleNumberTechnique);
-			}
-		}
-	}
+          return TechniqueResult(applicableCells: [SudokuCell(cell.row, cell.col, true, missingNums.first)], usedTechnique: LastPossibleNumberTechnique);
+        }
+      }
+    }
 		return null;
 	}
 }
